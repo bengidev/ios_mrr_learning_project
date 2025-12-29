cdf43b2f4eb701e0393cdb77b89b6eda7aa2b376
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2025-12-29 19:21:53 +0700

    Add coordinator protocol into Protocols

---
+++ MRR Project/Protocols/Coordinator.h
@@ -0,0 +1,33 @@
+//
+//  Coordinator.h
+//  MRR Project
+//
+//  Created by ENB Mac Mini on 29/12/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol Coordinator <NSObject>
+
+@required
+/// Array of child coordinators managed by this coordinator
+@property (nonatomic, retain) NSMutableArray<id<Coordinator> > *childCoordinators;
+
+/// The navigation controller used for pushing view controllers
+@property (nonatomic, retain) UINavigationController *navigationController;
+
+/// Starts the coordinator flow
+- (void)start;
+
+@optional
+/// Reference to parent coordinator (should be weak to avoid retain cycles)
+@property (nonatomic, assign, nullable) id<Coordinator> parentCoordinator;
+
+/// Called when this coordinator's flow is complete
+- (void)coordinatorDidFinish:(id<Coordinator>)coordinator;
+
+@end
+
+NS_ASSUME_NONNULL_END
